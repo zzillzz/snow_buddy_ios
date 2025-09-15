fb0865b4a781cb86f9261388c32a07f929434750
zzillzz <zillerahim3@gmail.com>
2025-09-15 16:03:00 +1000

    Initial Commit

---
+++ snow-buddy/snow_buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  snow_buddyApp.swift
+//  snow-buddy
+//
+//  Created by Zill-e-Rahim on 15/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct snow_buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ snow-buddyTests/snow_buddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  snow_buddyTests.swift
+//  snow-buddyTests
+//
+//  Created by Zill-e-Rahim on 15/9/2025.
+//
+
+import Testing
+@testable import snow_buddy
+
+struct snow_buddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
